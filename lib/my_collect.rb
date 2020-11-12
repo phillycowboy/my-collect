b5000a5f8bb07322f6f70a5dd1f0ac4923bc8ffe
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-11-11 20:55:13 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,32 @@
 
+def my_collect(array)
+first_names = []
+counter = 0 
+while
+    counter < array.length 
+    first_names << yield(array[counter])
+    counter += 1 
+    end
+    first_names
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+    name.split(" ").first
+end
+
+def lang_collect(array)
+languages = []
+i = 0 
+while
+    i < array.length
+    languages << array[i]
+    i += 1
+    end
+    languages
+end
+
+array = ["ruby", "javascript","python","objective-c"]
+lang_collect(array) do |lang|
+    lang.upcase
+end
